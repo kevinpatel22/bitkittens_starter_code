583900c086b082d2f5f97c351c46395c7d0b8ba8
Kevin <mail.kevinpatel@gmail.com>
2019-06-30 14:21:18 -0400

    add cat in boxes using axios

--- bitkittens/static/js/cats.js
+++ bitkittens/static/js/cats.js
@@ -1,2 +1,23 @@
-document.addEventListener("DOMContentLoaded", function() {
+
+document.addEventListener("DOMContentLoaded", function () {
+  const summon = document.querySelector('.summon-cats');
+  let catbox = document.querySelectorAll('.cat-box');
+
+  summon.addEventListener('click', function () {
+    axios.get('http://bitkittens.herokuapp.com/cats.json')
+    .then(response => {
+      let cats = response.data.cats;
+
+      catbox.forEach((box, index) => {
+        let image = document.createElement('img');
+        image.src = cats[index].photo;
+        image.alt = "Photo of " + cats[index].name;
+        if (box.childElementCount > 0) {
+          box.replaceChild(image, box.firstChild);
+        } else {
+          box.appendChild(image);
+        }
+      });
+    });
+  });
 });
